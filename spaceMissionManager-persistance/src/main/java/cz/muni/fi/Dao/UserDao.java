016e246317565245db9a7ba5d2dd2eb412ab7df8
jsmadis <jakub.smadis@gmail.com>
2017-10-20 18:38:17 +0200

    Implementation of UserDao.

--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/UserDao.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/UserDao.java
@@ -4,49 +4,62 @@ import cz.muni.fi.Entity.User;
 
 import java.util.List;
 
+/**
+ * Created by jsmadis.
+ *
+ * @author jsmadis
+ */
+
 public interface UserDao {
 
-	/**
-	 * 
-	 * @param user
-	 */
-	void addUser(User user);
+    /**
+     * Persist user into database
+     *
+     * @param user instance of user
+     */
+    void addUser(User user);
 
-	/**
-	 * 
-	 * @param user
-	 */
-	void deleteUser(User user);
+    /**
+     * Update user in database
+     *
+     * @param user Instance of user
+     */
+    void updateUser(User user);
 
-	List<User> findAllUsers();
+    /**
+     * Delete user from database
+     *
+     * @param user instance of user
+     */
+    void deleteUser(User user);
 
-	List<User> findAllAstronauts();
+    /**
+     * Find all users
+     *
+     * @return List of all users, null if none
+     */
+    List<User> findAllUsers();
 
-	/**
-	 * 
-	 * @param id
-	 */
-	User findUserById(Long id);
+    /**
+     * Find all astronauts
+     *
+     * @return list of
+     */
 
-	/**
-	 * 
-	 * @param user
-	 */
-	void updateUser(User user);
+    List<User> findAllAstronauts();
 
-	List<User> findAllAvailableAstronauts();
+    /**
+     * Find user by id
+     *
+     * @param id User id
+     * @return User instance of user
+     */
+    User findUserById(Long id);
 
-	/**
-	 * 
-	 * @param user
-	 */
-	void acceptMission(User user);
-
-	/**
-	 * 
-	 * @param user
-	 * @param explanation
-	 */
-	void rejectMission(User user, String explanation);
-
+    /**
+     * Find all available astronauts
+     *
+     * @return List of available astronauts
+     */
+    List<User> findAllAvailableAstronauts();
 }
--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/Entity/User.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Entity/User.java
@@ -1,17 +1,139 @@
 package cz.muni.fi.Entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
+/**
+ * Created by jsmadis
+ *
+ * @author jsmadis
+ */
+
+@Entity
+//In Derby, its forbiden to 'USER' is reserved keyword, we need to rename table
+@Table(name = "Users")
 public class User {
 
-	private String name;
-	private LocalDate birthDate;
-	private String username;
-	private String password;
-	private boolean isManager;
-	private int experienceLevel;
-	private boolean rejectedMission;
-	private String explanation;
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotNull
+    private String name;
+
+    @NotNull
+    private LocalDate birthDate;
+
+    @Column(nullable = false, unique = true)
+    @Pattern(regexp = ".+@.+\\....?")
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    private boolean isManager;
+
+    private int experienceLevel;
+
+
+    private boolean rejectedMission;
+
+    private String explanation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Mission mission;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String username) {
+        this.email = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isManager() {
+        return isManager;
+    }
+
+    public void setManager(boolean manager) {
+        isManager = manager;
+    }
+
+    public int getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public void setExperienceLevel(int experienceLevel) {
+        this.experienceLevel = experienceLevel;
+    }
+
+    public boolean isRejectedMission() {
+        return rejectedMission;
+    }
+
+    public void setRejectedMission(boolean rejectedMission) {
+        this.rejectedMission = rejectedMission;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public Mission getMission() {
+        return mission;
+    }
+
+    public void setMission(Mission mission) {
+        this.mission = mission;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return getEmail() != null ? getEmail().equals(user.getEmail()) : user.getEmail() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getEmail() != null ? getEmail().hashCode() : 0;
+    }
 }

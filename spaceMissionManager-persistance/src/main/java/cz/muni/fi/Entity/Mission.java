03ecfd3c208e07b10669fa682cbe36d55d2ac184
mlynarikj <jakub.mlynarik@gmail.com>
2017-10-25 17:16:11 +0200

    implemented MissionDao added comments and Get/Set methods to Mission
    added author
    
    hashCode and equals impl
    
    lazy fetch for users in mission

--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/Entity/Mission.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Entity/Mission.java
@@ -1,12 +1,112 @@
 package cz.muni.fi.Entity;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * @author mlynarikj
+ */
+
+@Entity
 public class Mission {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<User> astronauts = new ArrayList<User>();
 
 	private String destination;
 	private ZonedDateTime eta;
 	private String missionDescription;
-	private Long id;
 
+	private Boolean active;
+	private String result;
+	private LocalDate endDate;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public ZonedDateTime getEta() {
+		return eta;
+	}
+
+	public void setEta(ZonedDateTime eta) {
+		this.eta = eta;
+	}
+
+	public String getMissionDescription() {
+		return missionDescription;
+	}
+
+	public void setMissionDescription(String missionDescription) {
+		this.missionDescription = missionDescription;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public List<User> getAstronauts() {
+		return astronauts;
+	}
+
+	public void setAstronauts(List<User> astronauts) {
+		this.astronauts = astronauts;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Mission)) return false;
+
+		Mission mission = (Mission) o;
+
+		return getId() != null ? getId().equals(mission.getId()) : mission.getId() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return getId() != null ? getId().hashCode() : 0;
+	}
 }

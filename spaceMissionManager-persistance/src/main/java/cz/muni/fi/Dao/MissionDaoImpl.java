a73e7994d5cec4c2153c8c650562484a8d92fd55
mlynarikj <jakub.mlynarik@gmail.com>
2017-10-28 13:01:05 +0200

    implemented MissionDao and fixed Mission entity

---
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/MissionDaoImpl.java
@@ -0,0 +1,67 @@
+package cz.muni.fi.Dao;
+
+import cz.muni.fi.Entity.Mission;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author mlynarikj
+ */
+
+@Repository
+public class MissionDaoImpl implements MissionDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public void createMission(Mission mission) {
+		if (mission == null){
+			throw new IllegalArgumentException("mission is null");
+		}
+		entityManager.persist(mission);
+	}
+
+	@Override
+	public void cancelMission(Mission mission) {
+		if (mission == null){
+			throw new IllegalArgumentException("mission is null");
+		}
+		entityManager.remove(entityManager.merge(mission));
+	}
+
+	@Override
+	public List<Mission> findAllMissions() {
+		return entityManager.createQuery("select m from Mission m", Mission.class).getResultList();
+	}
+
+	@Override
+	public List<Mission> findAllMissions(boolean active) {
+		return entityManager.createQuery("select m from Mission m where active = :active", Mission.class)
+				.setParameter("active", active)
+				.getResultList();
+	}
+
+	@Override
+	public Mission findMissionById(Long id) {
+		try {
+			return entityManager.createQuery("select m from Mission m where id = :id", Mission.class)
+					.setParameter("id", id)
+					.getSingleResult();
+		}catch (NoResultException nre){
+			return null;
+		}
+	}
+
+	@Override
+	public void updateMission(Mission mission) {
+		if (mission == null){
+			throw new IllegalArgumentException("mission is null");
+		}
+		entityManager.merge(mission);
+	}
+}
--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/Entity/Mission.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Entity/Mission.java
@@ -1,12 +1,123 @@
 package cz.muni.fi.Entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * @author mlynarikj
+ */
+
+@Entity
 public class Mission {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<User> astronauts = new ArrayList<User>();
+
+	@NotNull
+	@Column(nullable = false, unique = true)
+	private String name;
 	private String destination;
 	private ZonedDateTime eta;
 	private String missionDescription;
-	private Long id;
 
+	private boolean active;
+	private String result;
+	private LocalDate endDate;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public ZonedDateTime getEta() {
+		return eta;
+	}
+
+	public void setEta(ZonedDateTime eta) {
+		this.eta = eta;
+	}
+
+	public String getMissionDescription() {
+		return missionDescription;
+	}
+
+	public void setMissionDescription(String missionDescription) {
+		this.missionDescription = missionDescription;
+	}
+
+	public boolean getActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public List<User> getAstronauts() {
+		return astronauts;
+	}
+
+	public void setAstronauts(List<User> astronauts) {
+		this.astronauts = astronauts;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Mission)) return false;
+
+		Mission mission = (Mission) o;
+
+		return getName() != null ? getName().equals(mission.getName()) : mission.getName() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return getName() != null ? getName().hashCode() : 0;
+	}
 }

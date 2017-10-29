c7529b7c72b43bb749926f163f9348c02d1b0bb7
VojtechBruza <vojtech.bruza@gmail.com>
2017-10-29 18:35:55 +0100

    Spacecraft fixes - dao interface javadoc

--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/SpacecraftDao.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/SpacecraftDao.java
@@ -7,28 +7,33 @@ import java.util.List;
 public interface SpacecraftDao {
 
 	/**
-	 * 
-	 * @param spacecraft
+	 * Create new entity in the database
+	 * @param spacecraft entity to be persisted
 	 */
 	void addSpacecraft(Spacecraft spacecraft);
 
 	/**
-	 * 
-	 * @param spacecraft
+	 * Remove entity from the database
+	 * @param spacecraft entity to be deleted
 	 */
 	void removeSpacecraft(Spacecraft spacecraft);
 
+	/**
+	 * Get list of all spacecrafts in the database
+	 * @return list of all spacecrafts in the database
+	 */
 	List<Spacecraft> findAllSpacecrafts();
 
 	/**
-	 * 
-	 * @param id
+	 * Find spacecraft by its id
+	 * @param id to search for in the database
+	 * @return reuquested spacecraft
 	 */
 	Spacecraft findSpacecraftById(Long id);
 
 	/**
-	 * 
-	 * @param spacecraft
+	 * Update this spacecraft in the database
+	 * @param spacecraft entity to be updated
 	 */
 	void updateSpacecraft(Spacecraft spacecraft);
 

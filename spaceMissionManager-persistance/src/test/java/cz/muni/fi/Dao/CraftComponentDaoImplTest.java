49ce0e83d970a6bb8b0c2d624a5f91733d6f6651
VojtechBruza <vojtech.bruza@gmail.com>
2017-10-30 14:57:42 +0100

    Craft component dao test

--- spaceMissionManager-persistance/src/test/java/cz/muni/fi/Dao/CraftComponentDaoImplTest.java
+++ spaceMissionManager-persistance/src/test/java/cz/muni/fi/Dao/CraftComponentDaoImplTest.java
@@ -2,6 +2,7 @@ package cz.muni.fi.Dao;
 
 import cz.muni.fi.ApplicationContext;
 import cz.muni.fi.Entity.CraftComponent;
+import cz.muni.fi.Entity.Spacecraft;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -11,7 +12,13 @@ import org.testng.annotations.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceUnit;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -27,15 +34,34 @@ public class CraftComponentDaoImplTest extends AbstractTestNGSpringContextTests 
     CraftComponent orbitalModule;
     CraftComponent heatShield;
     CraftComponent retroRocket;
+    Spacecraft spacecraft;
 
     @PersistenceUnit
     EntityManagerFactory entityManagerFactory;
 
     @BeforeMethod
 	public void setUp() {
+        spacecraft = new Spacecraft();
+        spacecraft.setName("S");
+        spacecraft.setType("Test");
+
         orbitalModule = new CraftComponent();
+        orbitalModule.setName("Orbital Module");
+        orbitalModule.setReadyDate(LocalDate.of(2000, Month.JANUARY, 1));
+        orbitalModule.setReadyToUse(true);
+        orbitalModule.setSpacecraft(spacecraft);
+
         heatShield = new CraftComponent();
+        heatShield.setName("Heat Shield");
+        heatShield.setReadyDate(LocalDate.of(2020, Month.FEBRUARY, 1));
+        heatShield.setReadyToUse(false);
+        heatShield.setSpacecraft(spacecraft);
+
         retroRocket = new CraftComponent();
+        retroRocket.setName("Retro Rocket");
+        retroRocket.setReadyDate(LocalDate.of(2017, Month.MARCH, 1));
+        retroRocket.setReadyToUse(true);
+        retroRocket.setSpacecraft(spacecraft);
     }
 
     @AfterMethod
@@ -53,10 +79,10 @@ public class CraftComponentDaoImplTest extends AbstractTestNGSpringContextTests 
     public void testAddComponent(){
         EntityManager em = entityManagerFactory.createEntityManager();
         craftComponentDao.addComponent(orbitalModule);
-        assertThat(em.find(CraftComponent.class, orbitalModule.getID())).isEqualTo(orbitalModule);
+        assertThat(em.find(CraftComponent.class, orbitalModule.getId())).isEqualTo(orbitalModule);
         assertThat(em.createQuery("select c from CraftComponent c", CraftComponent.class).getResultList()).hasSize(1);
         craftComponentDao.addComponent(heatShield);
-        assertThat(em.find(CraftComponent.class, heatShield.getID())).isEqualTo(heatShield);
+        assertThat(em.find(CraftComponent.class, heatShield.getId())).isEqualTo(heatShield);
         assertThat(em.createQuery("select c from CraftComponent c", CraftComponent.class).getResultList()).hasSize(2);
     }
 
@@ -67,8 +93,9 @@ public class CraftComponentDaoImplTest extends AbstractTestNGSpringContextTests 
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testAddComponentWithXXXX(){
-        //TODO next tests
+    public void testAddComponentWithNullName() throws Exception {
+        orbitalModule.setName(null);
+        craftComponentDao.addComponent(orbitalModule);
     }
 
 //    FIND ALL
@@ -83,18 +110,53 @@ public class CraftComponentDaoImplTest extends AbstractTestNGSpringContextTests 
     }
 
 //    FIND BY ID
-    public void testFindComponentById(){
+    @Test
+	public void testFindComponentById() throws Exception {
+        craftComponentDao.addComponent(heatShield);
+        assertThat(craftComponentDao.findComponentById(heatShield.getId())).isEqualToComparingFieldByField(heatShield);
+    }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+	public void testFindComponentByIdWithNullId() throws Exception {
+        craftComponentDao.addComponent(retroRocket);
+        craftComponentDao.findComponentById(null);
+    }
+
+    @Test(expectedExceptions = NoResultException.class)
+	public void testFindNonexistingComponent() throws Exception {
+        craftComponentDao.addComponent(retroRocket);
+        craftComponentDao.findComponentById(retroRocket.getId()+1L);
     }
 
 //    UPDATE
+    @Test
     public void testUpdateComponent(){
-
+        craftComponentDao.addComponent(orbitalModule);
+        orbitalModule.setName("Large orbital module");
+        assertThat(craftComponentDao.findComponentById(orbitalModule.getId())).isEqualToComparingFieldByField(orbitalModule);
     }
 
-//    REMOVE
-    public void testRemoveComponent(){
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testUpdateComponentWithId(){
+        craftComponentDao.addComponent(orbitalModule);
+        orbitalModule.setId(1L);
+        craftComponentDao.updateComponent(orbitalModule);
+    }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testUpdateComponentWithNullName() throws Exception {
+        craftComponentDao.addComponent(orbitalModule);
+        orbitalModule.setName(null);
+        craftComponentDao.updateComponent(orbitalModule);
+    }
+
+
+    //    REMOVE
+    public void testRemoveComponent(){
+        craftComponentDao.addComponent(retroRocket);
+        assertThat(craftComponentDao.findAllComponents()).contains(retroRocket);
+        craftComponentDao.removeComponent(retroRocket);
+        assertThat(craftComponentDao.findAllComponents()).isEmpty();
     }
 
 }

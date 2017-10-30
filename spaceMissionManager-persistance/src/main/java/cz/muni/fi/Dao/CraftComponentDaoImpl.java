ad4753813ddc137b71f92b3435218f22b648f99f
jsmadis <jakub.smadis@gmail.com>
2017-10-30 22:06:03 +0100

    fixed fetch all properties in findById methods

--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/CraftComponentDaoImpl.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/Dao/CraftComponentDaoImpl.java
@@ -41,7 +41,7 @@ public class CraftComponentDaoImpl implements CraftComponentDao {
             throw new IllegalArgumentException("id is null");
         }
         try {
-            return entityManager.createQuery("select cc from CraftComponent cc join fetch cc.spacecraft where id = :id", CraftComponent.class)
+            return entityManager.createQuery("select cc from CraftComponent cc fetch all properties where id = :id", CraftComponent.class)
                     .setParameter("id", id)
                     .getSingleResult();
         } catch (NoResultException nre) {

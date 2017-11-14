8c275bdf8af4cf80231f704de0372ba3603d7f3f
chlupaccom <cizmar@chlupac.com>
2017-11-14 14:50:23 +0100

    Repair spacecraft test fail (#55)
    * CraftComponent Entity + Dao
    
    * CraftComponent Entity repaired!
    
    * Added "many to one" anotation to spacecraft attribute in CraftComponent entity.
    
    * CraftComponent Entity repaired!
    
    * Javadoc
    
    * Javadoc
    
    * Repair of CraftComponent equals.
    
    * Repair of the updateSpacecraft test.

--- spaceMissionManager-persistance/src/main/java/cz/muni/fi/entity/CraftComponent.java
+++ spaceMissionManager-persistance/src/main/java/cz/muni/fi/entity/CraftComponent.java
@@ -71,7 +71,7 @@ public class CraftComponent {
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+		if (o == null || !(o instanceof CraftComponent)) return false;
 
 		CraftComponent that = (CraftComponent) o;
 

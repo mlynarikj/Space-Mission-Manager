38ac12e1d417be3e9c334312eca6d995ebf3cfa8
jsmadis <jakub.smadis@gmail.com>
2017-11-26 12:55:35 +0100

    fix bean mapping

--- spaceMissionManager-service/src/main/java/cz/muni/fi/config/EntityMapping.java
+++ spaceMissionManager-service/src/main/java/cz/muni/fi/config/EntityMapping.java
@@ -14,10 +14,16 @@ import org.dozer.loader.api.TypeMappingOptions;
 public class EntityMapping extends BeanMappingBuilder {
 	@Override
 	protected void configure() {
-		mapping(User.class, UserDTO.class, TypeMappingOptions.mapNull(false));
-		mapping(Mission.class, MissionDTO.class, TypeMappingOptions.mapNull(false));
-		mapping(CraftComponent.class, CraftComponentDTO.class, TypeMappingOptions.mapNull(false));
-		mapping(Spacecraft.class, SpacecraftDTO.class, TypeMappingOptions.mapNull(false));
+		mapping(Mission.class, MissionDTO.class, TypeMappingOptions.mapNull(false))
+				.fields(field("astronauts").accessible(true), field("astronauts").accessible(true))
+				.fields(field("spacecrafts").accessible(true), field("spacecrafts").accessible(true));
+		mapping(CraftComponent.class, CraftComponentDTO.class, TypeMappingOptions.mapNull(false))
+				.fields(field("spacecraft").accessible(true), field("spacecraft").accessible(true));
+		mapping(Spacecraft.class, SpacecraftDTO.class, TypeMappingOptions.mapNull(false))
+				.fields(field("mission").accessible(true), field("mission").accessible(true))
+				.fields(field("components").accessible(true), field("components").accessible(true));
+		mapping(User.class, UserDTO.class, TypeMappingOptions.mapNull(false))
+				.fields(field("mission").accessible(true), field("mission").accessible(true));
 		//TODO add other mapping options in future
 	}
 }

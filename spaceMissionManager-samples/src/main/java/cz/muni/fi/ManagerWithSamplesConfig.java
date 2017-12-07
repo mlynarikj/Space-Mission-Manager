ba282ea9d346f1927503854ed707f93b1914e102
mlynarikj <jakub.mlynarik@gmail.com>
2017-12-08 00:19:43 +0100

    first setup

---
+++ spaceMissionManager-samples/src/main/java/cz/muni/fi/ManagerWithSamplesConfig.java
@@ -0,0 +1,28 @@
+package cz.muni.fi;
+
+
+import cz.muni.fi.config.ServiceConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Configuration
+@Import(ServiceConfiguration.class)
+public class ManagerWithSamplesConfig {
+
+	final static Logger log = LoggerFactory.getLogger(ManagerWithSamplesConfig.class);
+
+	@Autowired
+	SampleDataLoadingFacade sampleDataLoadingFacade;
+
+	@PostConstruct
+	public void dataLoading() throws IOException {
+		log.debug("dataLoading()");
+		sampleDataLoadingFacade.loadData();
+	}
+}

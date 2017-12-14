55d6f466ba98d1e4f0da1a45b3a0ac131409b4b2
mlynarikj <jakub.mlynarik@gmail.com>
2017-12-14 16:05:13 +0100

    security

---
+++ spaceMissionManager-rest/src/main/java/cz/muni/fi/ApiUris.java
@@ -0,0 +1,8 @@
+package cz.muni.fi;
+
+public class ApiUris {
+
+	public static final String ROOT_URI_CRAFTCOMPONENTS = "/craftComponents";
+	//public static final String USER_CRAFTCOMPONENTS = "/user/craftComponents";
+
+}
---
+++ spaceMissionManager-rest/src/main/java/cz/muni/fi/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package cz.muni.fi.config;
+
+import cz.muni.fi.dto.UserDTO;
+import cz.muni.fi.facade.UserFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Import(ServiceConfiguration.class)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+	@Autowired
+	private UserFacade userFacade;
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.anyRequest()
+				.fullyAuthenticated()
+		.and()
+		.exceptionHandling().accessDeniedPage("/AccessDenied");
+		http.httpBasic();
+		http.csrf().disable();
+//				.antMatchers("/", "/home").permitAll()
+//				.antMatchers("/**").hasRole("MANAGER");
+//				.antMatchers("/user/**").hasRole("USER")
+//				.and()
+//				.formLogin()
+//				.loginPage("/login")
+//				.permitAll()
+//				.and()
+//				.logout()
+//				.permitAll();
+	}
+
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		for (UserDTO p : userFacade.findAllUsers()) {
+			auth.inMemoryAuthentication()
+					.withUser(p.getEmail())
+					.password(p.getPassword())
+					.roles(p.isManager() ? "MANAGER" : "USER");
+		}
+	}
+
+
+}

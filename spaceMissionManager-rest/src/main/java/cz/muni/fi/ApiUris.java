34f3fc04ea50b04f14bb6fa951f72f6cba531da0
VojtechBruza <vojtech.bruza@gmail.com>
2017-12-17 22:23:14 +0100

    Mission rest and jackson annotations (#99)
    mission rest controller + jackson annotations

--- spaceMissionManager-rest/src/main/java/cz/muni/fi/ApiUris.java
+++ spaceMissionManager-rest/src/main/java/cz/muni/fi/ApiUris.java
@@ -2,6 +2,7 @@ package cz.muni.fi;
 
 public class ApiUris {
 
+	public static final String ROOT_URI_MISSIONS = "/missions";
 	public static final String ROOT_URI_CRAFTCOMPONENTS = "/craftComponents";
 	//public static final String USER_CRAFTCOMPONENTS = "/user/craftComponents";
 	public static final String ROOT_URI_USERS = "/users";

c7d0d0e639c199735a6fbbee02fca0da4454ed7d
jenik <cizmar@chlupac.com>
2017-12-17 13:51:09 +0100

    Added spacecraft rest

--- spaceMissionManager-rest/src/main/java/cz/muni/fi/controllers/SpacecraftController.java
+++ spaceMissionManager-rest/src/main/java/cz/muni/fi/controllers/SpacecraftController.java
@@ -2,86 +2,80 @@ package cz.muni.fi.controllers;
 
 
 import cz.muni.fi.ApiUris;
-import cz.muni.fi.dto.UserCreateDTO;
-import cz.muni.fi.dto.UserDTO;
+import cz.muni.fi.dto.SpacecraftCreateDTO;
+import cz.muni.fi.dto.SpacecraftDTO;
 import cz.muni.fi.exceptions.ResourceAlreadyExistsException;
 import cz.muni.fi.exceptions.ResourceNotFoundException;
-import cz.muni.fi.facade.UserFacade;
+import cz.muni.fi.facade.SpacecraftFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 import java.util.Collection;
 
 /**
- * Created by jsmadis
+ * Created by jcizmar
  *
- * @author jsmadis
+ * @author jcizmar
  */
 
 @RestController
-@RequestMapping(ApiUris.ROOT_URI_USERS)
-public class UsersController {
+@RequestMapping(ApiUris.ROOT_URI_SPACECRAFTS)
+public class SpacecraftController {
 
-    private final static Logger logger = LoggerFactory.getLogger(UsersController.class);
+    private final static Logger logger = LoggerFactory.getLogger(SpacecraftController.class);
 
 
     @Autowired
-    private UserFacade userFacade;
+    private SpacecraftFacade spacecraftFacade;
 
     /**
-     * Returns all users
-     * @return Collection of UserDTOs
+     * Returns all spacecrafts
+     * @return SpacecraftDTO Collection of Spacecrafts
      */
 
     @RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<UserDTO> getUsers() {
-        logger.debug("[REST] getUsers()");
-        return userFacade.findAllUsers();
-
+    public Collection<SpacecraftDTO> getUsers() {
+        logger.debug("[REST] getSpacecraft()");
+        return spacecraftFacade.findAllSpacecrafts();
     }
 
     /**
-     * Returns user by ID
-     * @param id id of user
-     * @return UserDTO
+     * Returns spacecrat by id
+     * @param id id of spacecraft
+     * @return SpacecraftDTO
      */
-
     @RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO getUser(@PathVariable("id") long id) {
-        logger.debug("[REST] getUser() id=" + id);
-        UserDTO userDTO = userFacade.findUserById(id);
-        if (userDTO == null) {
+    public SpacecraftDTO getUser(@PathVariable("id") long id) {
+        logger.debug("[REST] getSpacecraft() id=" + id);
+        SpacecraftDTO spacecraftDTO = spacecraftFacade.findSpacecraftById(id);
+        if (spacecraftDTO == null) {
             throw new ResourceNotFoundException();
         }
-        return userDTO;
+        return spacecraftDTO;
     }
 
     /**
-     * Creates and returns user
-     * @param user UserCreateDTO
-     * @return UserDTO
+     * Creates and returns spacecraft
+     * @param spacecraft Spacecraft to create
+     * @return SpacecraftDTO created spacecraft
      */
 
     @RolesAllowed("ROLE_MANAGER")
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO createUser(@RequestBody UserCreateDTO user) {
+    public SpacecraftDTO crateSpacecraft(@RequestBody SpacecraftCreateDTO spacecraft) {
 
-        logger.debug("[REST] createUser()");
+        logger.debug("[REST] createSpacecraft()");
 
         try {
-            Long id = userFacade.addUser(user);
-            return userFacade.findUserById(id);
+            Long id = spacecraftFacade.addSpacecraft(spacecraft);
+            return spacecraftFacade.findSpacecraftById(id);
         } catch (Exception e) {
             logger.warn(e.getMessage());
             throw new ResourceAlreadyExistsException();
@@ -89,113 +83,40 @@ public class UsersController {
     }
 
     /**
-     * Deletes user and returns all remaining users
-     * @param id
-     * @return
+     * Deletes spacecraft and returns all remaining spacecrafts
+     * @param id id of spacecraft to delete
+     * @return Collection<SpacecraftDTO> all remaining spacecrafts
      */
 
     @RolesAllowed("ROLE_MANAGER")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public Collection<UserDTO> deleteUser(@PathVariable Long id) {
-        logger.debug("[REST] deleteUser()");
+    public Collection<SpacecraftDTO> deleteSpacecraft(@PathVariable Long id) {
+        logger.debug("[REST] deleteSpacecraft()");
 
-        UserDTO user = userFacade.findUserById(id);
-        if (user == null) {
+        SpacecraftDTO spacecraft = spacecraftFacade.findSpacecraftById(id);
+        if (spacecraft == null) {
             throw new ResourceNotFoundException();
         }
-        userFacade.deleteUser(user);
-        return userFacade.findAllUsers();
+        spacecraftFacade.removeSpacecraft(spacecraft);
+        return spacecraftFacade.findAllSpacecrafts();
     }
 
     /**
-     * Returns all astronauts
-     * @return collection of UserDTOs
+     * Updates and returns spacecraft
+     * @param spacecraft spacecraft to update
+     * @return SpacecraftDTO updated spacecraft
      */
-
-    @RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
-    @RequestMapping(value = "/astronauts", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<UserDTO> getAstronauts() {
-        logger.debug("[REST] getAstronauts()");
-
-        return userFacade.findAllAstronauts();
-    }
-
-    /**
-     * Returns all available astronauts
-     * @return collection of UserDTOs
-     */
-
-    @RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
-    @RequestMapping(value = "/astronauts/available", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<UserDTO> getAvailableAstronauts() {
-        logger.debug("[REST] getAvailableAstronauts()");
-
-        return userFacade.findAllAvailableAstronauts();
-    }
-
-    /**
-     * Updates and returns user
-     * @param user UserDTO
-     * @return UserDTO
-     */
-
     @RolesAllowed("ROLE_MANAGER")
     @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO updateUser(@RequestBody UserDTO user) {
-        logger.debug("[REST] updateUser()");
+    public SpacecraftDTO updateSpacecraft(@RequestBody SpacecraftDTO spacecraft) {
+        logger.debug("[REST] updateSpacecraft()");
 
         try {
-            userFacade.updateUser(user);
-            return user;
+            spacecraftFacade.updateSpacecraft(spacecraft);
+            return spacecraft;
         } catch (Exception e) {
             logger.warn(e.getMessage());
             throw new ResourceAlreadyExistsException();
         }
     }
-
-    /**
-     * Accepts mission for user
-     * @param id id of user
-     * @return UserDTO
-     */
-
-    @RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
-    @RequestMapping(value = "/{id}/acceptMission", method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO acceptMission(@PathVariable Long id) {
-        logger.debug("[REST] acceptMission()");
-
-        UserDTO user = userFacade.findUserById(id);
-        if (user == null) {
-            throw new ResourceNotFoundException();
-        }
-
-        userFacade.acceptAssignedMission(user);
-        return user;
-    }
-
-    /**
-     * Rejects mission for user
-     * @param id id of user
-     * @param explanation explanation of user
-     * @return UserDTO
-     */
-
-    @RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
-    @RequestMapping(value = "{id}/rejectMission", method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO rejectMission(@PathVariable Long id, @RequestBody String explanation) {
-        logger.debug("[REST] rejectMission");
-
-        UserDTO user = userFacade.findUserById(id);
-        if (user == null) {
-            throw new ResourceNotFoundException();
-        }
-        userFacade.rejectAssignedMission(user, explanation);
-        return user;
-    }
-
-
 }

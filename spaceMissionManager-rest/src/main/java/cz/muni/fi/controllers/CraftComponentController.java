b8d66f75fbe845cec7846e2cc323d13378be1b10
mlynarikj <jakub.mlynarik@gmail.com>
2017-12-18 14:25:48 +0100

    cc added new method

--- spaceMissionManager-rest/src/main/java/cz/muni/fi/controllers/CraftComponentController.java
+++ spaceMissionManager-rest/src/main/java/cz/muni/fi/controllers/CraftComponentController.java
@@ -15,6 +15,7 @@ import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(ApiUris.ROOT_URI_CRAFTCOMPONENTS)
@@ -34,11 +35,11 @@ public class CraftComponentController {
 	@RolesAllowed("ROLE_MANAGER")
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public CraftComponentDTO createCraftComponent(@RequestBody CraftComponentCreateDTO craftComponentCreateDTO) throws Exception{
+	public CraftComponentDTO createCraftComponent(@RequestBody CraftComponentCreateDTO craftComponentCreateDTO) throws Exception {
 		logger.log(Level.INFO, "[REST] Creating CraftComponent...");
 		try {
 			return craftComponentFacade.findComponentById(craftComponentFacade.addComponent(craftComponentCreateDTO));
-		}catch (Exception e){
+		} catch (Exception e) {
 			logger.log(Level.WARNING, e.getMessage());
 			throw new ResourceAlreadyExistsException();
 		}
@@ -46,17 +47,17 @@ public class CraftComponentController {
 
 	@RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<CraftComponentDTO> findAllComponents(){
+	public List<CraftComponentDTO> findAllComponents() {
 		logger.log(Level.INFO, "[REST] finding all components...");
 		return craftComponentFacade.findAllComponents();
 	}
 
 	@RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public CraftComponentDTO findComponentById(@PathVariable("id") Long id){
-		logger.log(Level.INFO, "[REST] finding component "+id+"...");
-		CraftComponentDTO componentDTO =  craftComponentFacade.findComponentById(id);
-		if (componentDTO == null){
+	public CraftComponentDTO findComponentById(@PathVariable("id") Long id) {
+		logger.log(Level.INFO, "[REST] finding component " + id + "...");
+		CraftComponentDTO componentDTO = craftComponentFacade.findComponentById(id);
+		if (componentDTO == null) {
 			throw new ResourceNotFoundException();
 		}
 		return componentDTO;
@@ -64,16 +65,16 @@ public class CraftComponentController {
 
 	@RolesAllowed({"ROLE_MANAGER"})
 	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public CraftComponentDTO updateComponent(@RequestBody CraftComponentDTO craftComponentDTO){
-		logger.log(Level.INFO, "[REST] updating component"+craftComponentDTO.getId()+"...");
-		CraftComponentDTO componentDTO =  craftComponentFacade.findComponentById(craftComponentDTO.getId());
-		if (componentDTO == null){
+	public CraftComponentDTO updateComponent(@RequestBody CraftComponentDTO craftComponentDTO) {
+		logger.log(Level.INFO, "[REST] updating component" + craftComponentDTO.getId() + "...");
+		CraftComponentDTO componentDTO = craftComponentFacade.findComponentById(craftComponentDTO.getId());
+		if (componentDTO == null) {
 			throw new ResourceNotFoundException();
 		}
 		try {
 			craftComponentFacade.updateComponent(craftComponentDTO);
 			return craftComponentFacade.findComponentById(componentDTO.getId());
-		}catch (Exception e){
+		} catch (Exception e) {
 			logger.log(Level.WARNING, e.getMessage());
 			throw new ResourceAlreadyExistsException();
 		}
@@ -82,13 +83,21 @@ public class CraftComponentController {
 	@RolesAllowed({"ROLE_MANAGER"})
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<CraftComponentDTO> removeComponent(@PathVariable("id") long id) throws Exception {
-		logger.log(Level.INFO, "[REST] deleting component"+id+"...");
+		logger.log(Level.INFO, "[REST] deleting component" + id + "...");
 		CraftComponentDTO component = craftComponentFacade.findComponentById(id);
-		if (component == null){
+		if (component == null) {
 			throw new ResourceNotFoundException();
 		}
 		craftComponentFacade.removeComponent(component);
 		return craftComponentFacade.findAllComponents();
+	}
+
+
+	@RolesAllowed({"ROLE_MANAGER", "ROLE_USER"})
+	@RequestMapping(value = "/available", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<CraftComponentDTO> findAllAvailableComponents() {
+		logger.log(Level.INFO, "[REST] finding all available components...");
+		return craftComponentFacade.findAllComponents().stream().filter(p -> p.getSpacecraft() == null).collect(Collectors.toList());
 	}
 
 

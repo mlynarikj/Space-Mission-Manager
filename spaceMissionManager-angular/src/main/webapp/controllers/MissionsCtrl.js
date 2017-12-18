1817f755f571d028a2770ddc814455d24658e907
VojtechBruza <vojtech.bruza@gmail.com>
2017-12-18 14:49:59 +0100

    mission ui service

--- spaceMissionManager-angular/src/main/webapp/controllers/MissionsCtrl.js
+++ spaceMissionManager-angular/src/main/webapp/controllers/MissionsCtrl.js
@@ -1,3 +1,49 @@
-controllers.controller('MissionsCtrl', function ($scope, $spaceHttp) {
-    console.log('calling  /components');
-  });
+controllers.controller('MissionsCtrl', function ($scope, $spaceHttp, $rootScope, $location) {
+
+    if (typeof $rootScope.globals === 'undefined' || typeof $rootScope.globals.currentUser === 'undefined') {
+        $location.path('login');
+        return;
+    }
+
+    console.log('calling  /missions');
+    $spaceHttp.loadMissions().then(function (response) {
+        console.log(response);
+        $scope.missions = response.data;
+    });
+    $spaceHttp.loadAstronauts().then(function (response) {
+        console.log(response);
+        $scope.astronauts = response.data;
+    });
+    $spaceHttp.loadSpacecrafts().then(function (response) {
+        console.log(response);
+        $scope.spacecrafts = response.data;
+    });
+
+    $scope.createMission = function () {
+        $scope.newMission =  {
+            'name': '',
+            'destination': '',
+            'active': true,
+            'astronauts': [],
+            'spacecrafts': [],
+            'eta': '',
+            'missionDescription': '',
+            'endDate': ''
+        };
+        $scope.create = true;
+    };
+
+    $scope.submitCreate = function () {
+        var data = angular.copy($scope.newMission);
+        $spaceHttp.createMission(data).then(function (res) {
+            $spaceHttp.loadMissions().then(function (response) {
+                $scope.missions = response.data;
+                $scope.create = false;
+            }, function (error) {
+                console.error(error);
+            });
+        }, function (error) {
+            console.error(error);
+        })
+    };
+});
--- spaceMissionManager-angular/src/main/webapp/service.js
+++ spaceMissionManager-angular/src/main/webapp/service.js
@@ -4,12 +4,16 @@ spaceMissionApp.factory('$spaceHttp', ['$http', function ($http) {
     const API_URL = "http://localhost:8080/pa165/rest/";
     const LOGIN_PATH = "login";
     const USERS_PATH = "users";
+    const MISSIONS_PATH = "missions";
+    const ASTRONAUTS_PATH = "astronauts";
+    const SPACECRAFTS_PATH = "spacecrafts";
 
 
     service.login = function (name, password) {
         return $http.post(API_URL+LOGIN_PATH, {name: name, password: password});
     };
 
+    //Users
     service.getAllUsers = function () {
         return $http.get(API_URL+USERS_PATH);
     };
@@ -32,6 +36,10 @@ spaceMissionApp.factory('$spaceHttp', ['$http', function ($http) {
         return $http.post(API_URL+USERS_PATH, data);
     };
 
+    service.loadAstronauts = function () {
+        return $http.get(API_URL+ASTRONAUTS_PATH);
+    };
+
     service.acceptMission = function (id) {
         return $http.get(API_URL+USERS_PATH+'/'+id+'/acceptMission');
     };
@@ -39,6 +47,33 @@ spaceMissionApp.factory('$spaceHttp', ['$http', function ($http) {
     service.declineMission = function (id, message) {
         return $http.post(API_URL+USERS_PATH+'/'+id+'/rejectMission', message);
     };
+
+    //Missions
+    service.createMission = function (data){
+        return $http.post(API_URL+MISSIONS_PATH, data);
+    };
+
+    service.loadMissions = function () {
+        return $http.get(API_URL+MISSIONS_PATH);
+    };
+
+    service.deleteMission = function (id) {
+        return $http.delete(API_URL+MISSIONS_PATH+'/'+id);
+    };
+
+    service.getMission = function (id) {
+        return $http.get(API_URL+MISSIONS_PATH+'/'+id);
+    };
+
+    service.updateMission = function (data){
+        return $http.put(API_URL+MISSIONS_PATH, data);
+    };
+
+    //Spacecrafts
+    service.loadSpacecrafts = function () {
+        return $http.get(API_URL+SPACECRAFTS_PATH);
+    };
+
 
     return service;
 }]);

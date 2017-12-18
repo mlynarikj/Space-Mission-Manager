233019b0f6051a1b91e1c788098a26f00707f38a
VojtechBruza <vojtech.bruza@gmail.com>
2017-12-18 21:34:28 +0100

    mission ui (#105)
    mission UI

--- spaceMissionManager-angular/src/main/webapp/controllers/MissionsCtrl.js
+++ spaceMissionManager-angular/src/main/webapp/controllers/MissionsCtrl.js
@@ -1,3 +1,102 @@
-controllers.controller('MissionsCtrl', function ($scope, $spaceHttp) {
-    console.log('calling  /components');
-  });
+controllers.controller('MissionsCtrl', function ($scope, $spaceHttp, $rootScope, $location) {
+
+    if (typeof $rootScope.globals === 'undefined' || typeof $rootScope.globals.currentUser === 'undefined') {
+        $location.path('login');
+        return;
+    }
+
+    console.log('calling  /missions');
+    $spaceHttp.loadMissions().then(function (response) {
+        console.log(response);
+        $scope.missions = response.data;
+    });
+    $spaceHttp.loadAstronauts().then(function (response) {
+        console.log(response);
+        $scope.astronauts = response.data;
+    });
+    $spaceHttp.loadSpacecrafts().then(function (response) {
+        console.log(response);
+        $scope.spacecrafts = response.data;
+    });
+
+    $scope.createNewMission = function () {
+        $scope.editedMission =  {
+            'active': true,
+            'astronauts': [],
+            'spacecrafts': []
+        };
+        $scope.create = true;
+    };
+
+    $scope.submitCreate = function () {
+        var data = angular.copy($scope.editedMission);
+        var selectedAstronauts = $('#astronauts').val();
+        var selectedSpacecrafts = $('#spacecrafts').val();
+        $scope.astronauts.forEach(function(astronaut){
+            selectedAstronauts.forEach(function(index){
+                if(index == astronaut.id){
+                    console.log(astronaut);
+                    data.astronauts.push(astronaut);
+                }
+            });
+        });
+        $scope.spacecrafts.forEach(function(spacecraft){
+            selectedSpacecrafts.forEach(function(index){
+                if(index == spacecraft.id){
+                    console.log(spacecraft);
+                    data.spacecrafts.push(spacecraft);
+                }
+            });
+        });
+        data.eta = data.eta.toISOString().substring(0, 10);
+        console.log(data);
+        $spaceHttp.createMission(data).then(function (res) {
+            $spaceHttp.loadMissions().then(function (response) {
+                $scope.missions = response.data;
+                $scope.create = false;
+            }, function (error) {
+                console.error(error);
+            });
+        }, function (error) {
+            console.error(error);
+        })
+    };
+
+    $scope.deleteMission = function (id) {
+        $spaceHttp.deleteMission(id).then(function (response) {
+            $scope.missions = response.data;
+        }, function (error) {
+            console.error(error);
+        });
+    };
+
+    $scope.editMission = function (id) {
+        $spaceHttp.getMission(id).then(function (response) {
+            $scope.editedMission = response.data;
+            $scope.editedMission.eta = new Date(response.data.eta);
+            $scope.edit = true;
+        }, function (error) {
+            console.error(error);
+        })
+    };
+
+    $scope.cancelEdit = function () {
+        $scope.create = false;
+        $scope.edit = false;
+    }
+
+    $scope.submitEdit = function () {
+        var data = angular.copy($scope.editedMission);
+        data.eta = data.eta.toISOString().substring(0, 10);
+        $spaceHttp.updateMission(data).then(function (res) {
+            $spaceHttp.getAllMissions().then(function (response) {
+                $scope.missions = response.data;
+                $scope.edit = false;
+            }, function (error) {
+                console.error(error);
+            });
+        }, function (error) {
+            console.error(error);
+        })
+    };
+});
